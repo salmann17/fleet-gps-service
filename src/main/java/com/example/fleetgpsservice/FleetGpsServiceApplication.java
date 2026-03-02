1a08976cb84d3cc5ba22c300765a023bf220c1c2
Salman Alfarizi <salmanlfaa11@gmail.com>
2026-03-03 04:46:17 +0700

    first commit

---
+++ src/main/java/com/example/fleetgpsservice/FleetGpsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fleetgpsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetGpsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetGpsServiceApplication.class, args);
+	}
+
+}
